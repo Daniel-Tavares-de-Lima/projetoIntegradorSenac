9ed9f739331688075934b5db57caacb7fb4c9e05
Daniel Tavares de Lima Marcelino <toys.tokio@gmail.com>
2025-05-06 19:29:49 -0300

    Reverter a merda do estagiário

--- src/app/casos/page.tsx
+++ src/app/casos/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import Image from "next/image";
@@ -257,55 +256,16 @@ export default function Casos() {
               <TbFileSearch className={casosStyles.iconeInterno} />
               <Link href={`/evidencias`} className={casosStyles.link}>Evidências</Link>
             </div>
-=======
-// import Image from "next/image";
-// import styles from "./page.module.css";
-import casosStyles from "../styles/Home.module.css";
-import Link from "next/link";
-// import {useState} from "react";
-
-export default function Casos() {
-  return (
-    <div className={casosStyles.container}>
-
-      {/*--------SIDEBAR ESQUERDA--------------------------*/}
-      <aside className={casosStyles.sidebar}>
-        {/* <div className={casosStyles.logo}>Laudos Periciais Odonto-Legal</div> */}
-        <div className="logo">
-          <div className="imagem">  
-          </div>
-
-          <div className="titulo">
-            <h1>Laudos Periciais Odonto-Legal</h1>
-          </div>
-
-
-          <nav className={casosStyles.navi}>
-            <Link href={`/pacientes`} className={casosStyles.link}>Pacientes</Link>
-            <Link href={`/cadastros`} className={casosStyles.link}>Cadastros</Link>
-            <Link href={`/profissionais`} className={casosStyles.link}>Profissionais</Link>
-            <Link href={`/casos`} className={casosStyles.link}>Casos</Link>
-            <Link href={`/evidencias`} className={casosStyles.link}>Evidências</Link>
->>>>>>> origin/roni-branch
           </nav>
         </div>
         <div className={casosStyles.config}>⚙️ Configurações</div>
       </aside>
 
-<<<<<<< HEAD
       <main className={casosStyles.main}>
-=======
-      {/*--------SIDEBAR ESQUERDA--------------------------*/}
-
-      <main className={casosStyles.main}>
-
-        {/*--------------HEADER-----------------------*/}
->>>>>>> origin/roni-branch
         <header className={casosStyles.header}>
           <div className={casosStyles.logoApp}>
             Gest<span>Odo</span>
           </div>
-<<<<<<< HEAD
           <input
             type="search"
             placeholder="Pesquisar por caso"
@@ -525,209 +485,9 @@ export default function Casos() {
                 )}
               </tbody>
             </table>
-=======
-
-          <input type="search" placeholder="Pesquisar casos ou pacientes" className={casosStyles.pesquisa}/>
-
-          {/*-----O usuário também tem que vim do backend----*/}
-          <div className={casosStyles.user}>👤 Julia</div>
-        </header>
-        {/*--------------HEADER-----------------------*/}
-
-        <section className={casosStyles.content}>
-          <h1>Casos</h1>
-
-          {/*-------------PESQUISAR POR CASOS---------------*/}
-
-          <h2>Pesquisar</h2>
-          <input type="search" placeholder="Pesquisar casos ou pacientes" className={casosStyles.pesquisa}/>
-
-
-          <div className={casosStyles.conteudo}>
-            <button className={casosStyles.botaoPesquisar}>🔍 Pesquisar</button>
-    
-          </div>
-          {/*-------------CADASTRAR CASOS---------------*/}
-
-          <div className={casosStyles.section}>
-
-            {/*-------------TABELA DE CADASTRAR CASOS---------------*/}
-            {/*-----------TODOS ESSES DADOS SÃO DE EXEMPLOS, OS VERDADEIROS TERÃO QUE VIM DO BACKEND*/}
-            <h2>Cadastrar Casos</h2>
-
-            <div className={casosStyles.cadastroCasos}>
-
-              <div className={casosStyles.cadastroEsquerda}>
-                <div className={casosStyles.organizacao}>
-                  <label> Titulo do Caso* <br />
-                    <input type="text" placeholder="Digite o titulo do caso" required/>
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label> Descrição* <br />
-                    <textarea placeholder="Descreva os detalhes do caso" required></textarea>
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label>Data do Fato: <br />
-                    {/* <select>
-                      <option value="">Radiográfico</option>
-                      <option value="">Opção 2</option>
-                      <option value="">Opção 3</option>
-                      <option value="">Opção 4</option>
-                    </select> */}
-                    <input type="date" required />
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label> Local <br />
-                    {/* <select>
-                      <option value="">Exame odontologico comparativo</option>
-                      <option value="">Opção 2</option>
-                      <option value="">Opção 3</option>
-                      <option value="">Opção 4</option>
-                    </select> */}
-                    <input type="text" placeholder="Ex.: Recife, PE" required />
-                  </label>
-                </div>
-
-              </div>
-
-
-              <div className={casosStyles.cadastroDireita}>
-                {/*---------EXEMPLOS----------*/}
-              <div className={casosStyles.organizacao}>
-                <label>Tipo de Caso: <br />
-                    <select>
-                      <option value="">Acidente</option>
-                      <option value="">Opção 2</option>
-                      <option value="">Opção 3</option>
-                      <option value="">Opção 4</option>
-                    </select>
-                  </label>
-              </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label>Perito Responsável: 
-                    <input type="text" placeholder="Nome do solicitante" />
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label>Atribuir Assistente: <br />
-                    <select>
-                      <option value="">Nenhum</option>
-                      <option value="">Opção 2</option>
-                      <option value="">Opção 3</option>
-                      <option value="">Opção 4</option>
-                    </select>
-                  </label>
-                </div>
-
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label> Status <br />
-                    <select>
-                      <option value="">Em andamento</option>
-                      <option value="">Finalizado</option>
-                      <option value="">Arquivado</option>
-                    </select>
-                  </label>
-                </div>
-
-                <button>Salvar</button>
-              </div>
-            </div>
-          
-          {/*-------------TABELA DE CASOS---------------*/}
-          {/*-----------TODOS ESSES DADOS SÃO DE EXEMPLOS, OS VERDADEIROS TERÃO QUE VIM DO BACKEND*/}
-          
-            <h2>Todos os Casos</h2>
-            <table>
-    <thead>
-      <tr>
-        <th>Código</th>
-        <th>Tipo</th>
-        <th>Data do Fato</th>
-        <th>Local</th>
-        <th>Solicitante da Perícia</th>
-        <th>Responsável</th>
-        <th>Data do Exame</th>
-        <th>Últimos Exames</th>
-        <th>Solicitar Exames</th>
-        <th>Status</th>
-        <th>Ações</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>#022</td>
-        <td>Acidente</td>
-        <td>12/03/25 - 12:43</td>
-        <td>📍 Recife-PE</td>
-        <td>Carlos Andrade</td>
-        <td>Julia Maria</td>
-        <td>14/02/23 - 12:03hrs</td>
-        <td>
-          02/22 11:25 AM Exame odontol... <a href="#" className={casosStyles.verTudo}>Ver Tudo</a>
-        </td>
-        <td>
-          <button className={casosStyles.botaoExame}>Solicitar Exame</button>
-        </td>
-        <td>
-          <span className={casosStyles.statusEmAndamento}>Em andamento</span>
-        </td>
-        <td className={casosStyles.acoes}>
-          <button className={casosStyles.acaoBotao} title="Confirmar">✅</button>
-          <button className={casosStyles.acaoBotao} title="Editar">✏️</button>
-          <button className={casosStyles.acaoBotao} title="Excluir">❌</button>
-        </td>
-      </tr>
-      <tr>
-        <td>#021</td>
-        <td>Acidente</td>
-        <td>05/03/25 - 10:33</td>
-        <td>📍 Jaboatão-PE</td>
-        <td>Marcos Silva</td>
-        <td>Julia Maria</td>
-        <td>17/12/22 - 19:14hrs</td>
-        <td>
-          11/23 09:12 AM Análise de arcada... <a href="#" className={casosStyles.verTudo}>Ver Tudo</a>
-        </td>
-        <td>
-          <button className={casosStyles.botaoExame}>Solicitar Exame</button>
-        </td>
-        <td>
-          <span className={casosStyles.statusArquivado}>Arquivado</span>
-        </td>
-        <td className={casosStyles.acoes}>
-          <button className={casosStyles.acaoBotao} title="Confirmar">✅</button>
-          <button className={casosStyles.acaoBotao} title="Editar">✏️</button>
-          <button className={casosStyles.acaoBotao} title="Excluir">❌</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-            {/*-------------TABELA DE PACIENTES---------------*/}
-            {/*-----------TODOS ESSES DADOS SÃO DE EXEMPLOS, OS VERDADEIROS TERÃO QUE VIM DO BACKEND*/}
-
->>>>>>> origin/roni-branch
           </div>
         </section>
       </main>
     </div>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/roni-branch
--- src/app/profissionais/page.tsx
+++ src/app/profissionais/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import Image from "next/image";
@@ -217,56 +216,16 @@ export default function Profissionais() {
               <TbFileSearch className={casosStyles.iconeInterno} />
               <Link href={`/evidencias`} className={casosStyles.link}>Evidências</Link>
             </div>
-=======
-// import Image from "next/image";
-// import styles from "./page.module.css";
-import casosStyles from "../styles/Home.module.css";
-import profissionaisStyles from "../styles/Profissionais.module.css";
-import Link from "next/link";
-// import {useState} from "react";
-
-export default function Profissionais() {
-  return (
-    <div className={casosStyles.container}>
-
-      {/*--------SIDEBAR ESQUERDA--------------------------*/}
-      <aside className={casosStyles.sidebar}>
-        {/* <div className={casosStyles.logo}>Laudos Periciais Odonto-Legal</div> */}
-        <div className="logo">
-          <div className="imagem">  
-          </div>
-
-          <div className="titulo">
-            <h1>Laudos Periciais Odonto-Legal</h1>
-          </div>
-
-
-          <nav className={casosStyles.navi}>
-            <Link href={`/pacientes`} className={casosStyles.link}>Pacientes</Link>
-            <Link href={`/cadastro`} className={casosStyles.link}>Cadastros</Link>
-            <Link href={`/profissionais`} className={casosStyles.link}>Profissionais</Link>
-            <Link href={`/casos`} className={casosStyles.link}>Casos</Link>
-            <Link href={`evidencias`} className={casosStyles.link}>Evidências</Link>
->>>>>>> origin/roni-branch
           </nav>
         </div>
         <div className={casosStyles.config}>⚙️ Configurações</div>
       </aside>
 
-<<<<<<< HEAD
       <main className={casosStyles.main}>
-=======
-      {/*--------SIDEBAR ESQUERDA--------------------------*/}
-
-      <main className={casosStyles.main}>
-
-        {/*--------------HEADER-----------------------*/}
->>>>>>> origin/roni-branch
         <header className={casosStyles.header}>
           <div className={casosStyles.logoApp}>
             Gest<span>Odo</span>
           </div>
-<<<<<<< HEAD
           <input
             type="search"
             placeholder="Pesquisar por usuário"
@@ -433,162 +392,9 @@ export default function Profissionais() {
                 )}
               </tbody>
             </table>
-=======
-
-          <input type="search" placeholder="Pesquisar casos ou pacientes" className={casosStyles.pesquisa}/>
-
-          {/*-----O usuário também tem que vim do backend----*/}
-          <div className={casosStyles.user}>👤 Julia</div>
-        </header>
-        {/*--------------HEADER-----------------------*/}
-
-        <section className={casosStyles.content}>
-          <h1>Profissionais</h1>
-
-          {/*-------------PESQUISAR POR CASOS---------------*/}
-
-          <h2>Pesquisar</h2>
-          <input type="search" placeholder="Pesquisar por usuário" className={casosStyles.pesquisa}/>
-
-
-          <div className={casosStyles.conteudo}>
-            <button className={casosStyles.botaoPesquisar}>🔍 Pesquisar</button>
-    
-          </div>
-          {/*-------------CADASTRAR CASOS---------------*/}
-
-          <div className={casosStyles.section}>
-
-            {/*-------------TABELA DE CADASTRAR CASOS---------------*/}
-            {/*-----------TODOS ESSES DADOS SÃO DE EXEMPLOS, OS VERDADEIROS TERÃO QUE VIM DO BACKEND*/}
-            <h2>Cadastrar profissionais</h2>
-
-            <div className={casosStyles.cadastroCasos} >
-
-              <div className={casosStyles.cadastroEsquerda}>
-                <div className={casosStyles.organizacao}>
-                  <label> Nome Completo <br />
-                    <input type="text" placeholder="Digite o nome completo" required/>
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label> E-mail* <br />
-                    <input type="email" placeholder="Digite o email" required/>
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label>Senha: <br />
-                    <input type="password" required placeholder="Digite a senha"/>
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-
-              </div>
-
-
-              <div className={casosStyles.cadastroDireita}>
-
-
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label> Matricula* <br />
-                    <input type="number" placeholder="Digite a matricula" required/>
-                  </label>
-                </div>
-
-                {/*---------EXEMPLOS----------*/}
-              <div className={casosStyles.organizacao}>
-                <label>Perfil de acesso: <br />
-                    <select>
-                      <option value="">Admin</option>
-                      <option value="">Perito</option>
-                      <option value="">Assistente</option>
-                    </select>
-                  </label>
-              </div>
-
-                {/*---------EXEMPLOS----------*/}
-                <div className={casosStyles.organizacao}>
-                  <label>Data de Nascimento: 
-                    <input type="date"  required />
-                  </label>
-                </div>
-
-                <button>Salvar</button>
-              </div>
-            </div>
-          
-          
-          {/*-----------TODOS ESSES DADOS SÃO DE EXEMPLOS, OS VERDADEIROS TERÃO QUE VIM DO BACKEND*/}
-          
-            <h2>Todos os profissionais</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Matricula</th>
-                        <th>Nome</th>
-                        <th>Data de Nascimento</th>
-                        <th>Nivel de acesso</th>
-                        {/* <th>Solicitante da Perícia</th>
-                        <th>Responsável</th>
-                        <th>Data do Exame</th>
-                        <th>Últimos Exames</th>
-                        <th>Solicitar Exames</th>
-                        <th>Status</th> */}
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>012345</td>
-                        <td>Julia Gomes Santana</td>
-                        <td>12/03/25</td>
-                        <td>
-                            <div className={profissionaisStyles.acessoADM}>
-                                <p>Administrador</p>
-                            </div>
-                        </td>
-                        <td className={casosStyles.acoes}>
-                            <button className={casosStyles.acaoBotao} title="Confirmar">✅</button>
-                            <button className={casosStyles.acaoBotao} title="Editar">✏️</button>
-                            <button className={casosStyles.acaoBotao} title="Excluir">❌</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>543210</td>
-                        <td>Marcelo Rodrigues Oliveira</td>
-                        <td>05/03/25</td>
-                        <td>
-                            <div className={profissionaisStyles.acessoPERITO}>
-                                <p>Perito</p>
-                            </div>
-                        </td>
-            
-                        <td className={casosStyles.acoes}>
-                            <button className={casosStyles.acaoBotao} title="Confirmar">✅</button>
-                            <button className={casosStyles.acaoBotao} title="Editar">✏️</button>
-                            <button className={casosStyles.acaoBotao} title="Excluir">❌</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            {/*-------------TABELA DE PACIENTES---------------*/}
-            {/*-----------TODOS ESSES DADOS SÃO DE EXEMPLOS, OS VERDADEIROS TERÃO QUE VIM DO BACKEND*/}
-
->>>>>>> origin/roni-branch
           </div>
         </section>
       </main>
     </div>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/roni-branch
